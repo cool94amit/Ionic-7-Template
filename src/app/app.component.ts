71a7cf590d9dad2112f45a53c5212f7300fae543
Amit Rai <arajanrai@gmail.com>
2024-01-06 20:47:06 +0530

    Initial Commit for Teomplate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonApp, IonRouterOutlet, Platform } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports: [IonApp, IonRouterOutlet],
+  imports: [
+    IonApp,
+    IonRouterOutlet,
+  ],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private platform: Platform) {
+
+    this.platform.backButton.subscribeWithPriority(10, (processNextHandler) => {
+      // processNextHandler(); 
+      console.log('Handler was called!');
+    });
+  }
 }
